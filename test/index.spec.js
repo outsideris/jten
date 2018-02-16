8ba021f31b3946e5fa1a47942a0938a9f7042dcd
Outsider <outsideris@gmail.com>
2018-02-16 21:27:06 +0900

    rearrange jten package name
    jate is old name before publishing it

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,18 +1,18 @@
 const { resolve } = require('path');
 const { expect } = require('chai');
 
-const jate = require('../lib');
+const jten = require('../lib');
 
-describe('jate', () => {
+describe('jten', () => {
   it('should render data to JSON', (done) => {
-    const path = resolve('./test/fixtures/example.jate');
+    const path = resolve('./test/fixtures/example.jten');
     const data = {
       isSuccess: true,
       id: 1234,
       name: 'blah blah',
     };
 
-    jate.__express(path, data, (err, result) => {
+    jten.__express(path, data, (err, result) => {
       expect(result).to.have.property('status').to.be.true;
       expect(result).to.have.property('data');
       expect(result.data).to.have.property('id').to.equal(1234);
