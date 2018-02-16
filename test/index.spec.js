0b990420a2fa5f68470df882608ffb2ea1a3336d
Outsider <outsideris@gmail.com>
2018-02-16 23:53:21 +0900

    clean null or undefined values

--- test/index.spec.js
+++ test/index.spec.js
@@ -13,11 +13,34 @@ describe('jten', () => {
     };
 
     jten.__express(path, data, (err, result) => {
+      if (err) { return done(err); }
+
       expect(result).to.have.property('status').to.be.true;
       expect(result).to.have.property('data');
       expect(result.data).to.have.property('id').to.equal(1234);
       expect(result.data).to.have.property('name').to.equal('blah blah');
-      done();
+      return done();
+    });
+  });
+
+  it('should remove fields when it\'s vaule is undefined or null', (done) => {
+    const path = resolve('./test/fixtures/example2.jten');
+    const data = {
+      isSuccess: false,
+      id: undefined,
+      name: 'blah blah',
+      users: [],
+    };
+
+    jten.__express(path, data, (err, result) => {
+      if (err) { return done(err); }
+
+      expect(result).to.have.property('status').to.be.false;
+      expect(result).to.have.property('data');
+      expect(result.data).to.have.not.property('id');
+      expect(result.data).to.have.property('name').to.equal('blah blah');
+      expect(result.data).to.have.property('users').to.have.lengthOf(0);
+      return done(err);
     });
   });
 });
